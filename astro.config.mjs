f212915e2f8680fb8fe887bf42e6cd2ff324f415
FanbetMedia <jani.k.naumanen@gmail.com>
2026-09-18 09:09:39 +0300

    Inline critical site CSS to remove render-blocking request

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-export default defineConfig({ site: "https://mgakasinot.com", trailingSlash: "always", integrations:[sitemap()] });
+
+export default defineConfig({
+  site: "https://mgakasinot.com",
+  trailingSlash: "always",
+  build: {
+    inlineStylesheets: "always",
+  },
+  integrations: [sitemap()],
+});
